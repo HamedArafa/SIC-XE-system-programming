c03f6c698783beaed5bc836807a081067cfd2b2e
hamedarafa <hamed.arafa.h@gmail.com>
2014-12-29 15:23:54 +0200

    InstrctionSet class added see comments to better understand

---
+++ SEC-XE/pass2/pass2.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include "../Shared/shared.cpp"
+int main ()
+{
+   InstructionSet instructionSet = InstructionSet();
+   instructionSet.initialize();
+}
